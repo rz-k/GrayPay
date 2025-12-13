8cebb3f84426ba65ee60434a2d17fa15f28e7250
reza <adamak.tnh@gmail.com>
2025-12-13 16:12:46 +0330

    add view get payment

--- apps/payment/views.py
+++ apps/payment/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views import View
 
-# Create your views here.
+from utils.load_env import env
+
+
+class PaymentView(View):
+    template_name = "payment/payment.html"
+
+    def get(self, request):
+        minimum_payment = int(env.MiNIMUM_PAYMENT)
+        context = {"minimum_payment": minimum_payment}
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        data = request.POST.get("my_field")
+        return redirect("same_view_name")
