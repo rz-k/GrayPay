c106f5637c43b88dbc6f7292db8cf2a1a535db16
reza <adamak.tnh@gmail.com>
2025-12-14 17:14:33 +0330

    update route

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('payment/', include('apps.payment.urls'))
+    path('', include('apps.payment.urls'))
 ]
