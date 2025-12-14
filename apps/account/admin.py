46d7c80a6ecb219f967f277ab8f2551a0801895d
reza <adamak.tnh@gmail.com>
2025-12-14 16:44:05 +0330

    added user in admin panel

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from apps.account.models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+
+    list_display = ("id", "email", "username", "first_name", "last_name", "is_active")
+    list_filter = ("is_active",)
+
+    fieldsets = (
+        (None, {
+            'fields': ('email',"username", 'password')
+        }),
+        ('Personal info', {
+            'fields': (
+                'first_name',
+                'last_name', 'phone_number')
+        }),
+        ('Permissions', {
+            'fields': (
+                'is_active', 'is_staff',
+                'is_superuser',
+                'groups', 'user_permissions')
+        })
+    )
+
+    search_fields = ('email', 'first_name',"username")
+    ordering = ('-id', 'email')
+    list_editable = ("username", "email")
